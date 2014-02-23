24ead42615cd182d1211da54c791558788946621
wara_be <c.mikamichel@gmail.com>
2014-02-23 16:04:34 +0900

    Add a StatisPages controller.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-SampleApp::Application.config.secret_key_base = '849175a38a23151cb9790cef19211f2be5c5284873c402cbfc0e3209aba37b7ed229587b3a54d2a95526310a63c92a10c2e46bbb6dff17dc8f38c912b3d8d5c6'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
